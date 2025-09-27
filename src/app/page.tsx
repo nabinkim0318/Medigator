5730193cb869e0d9aa05533a7cda81d483474178
milligansarah <sarahkm246@gmail.com>
2025-09-27 18:21:12 -0400

    Revamp Onboarding Questionaire

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,10 @@
 import { CedarCopilot } from "cedar-os";
 import AppointmentsPage from "./pages/AppointmentsPage";
 import PatientInterface from "./pages/PatientInterface";
+import OnboardingQuestionnaire from "./pages/OnboardingQuestionaire";
 
 export default function Home() {
   return (
-    <PatientInterface />
+    <OnboardingQuestionnaire />
   );
 }
