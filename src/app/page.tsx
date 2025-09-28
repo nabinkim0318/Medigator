ce19aefc93ca06dcc2726ae1c64b2b242cfb59ed
milligansarah <sarahkm246@gmail.com>
2025-09-27 20:36:43 -0400

    Medical history page complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,9 @@
 "use client";
 
-import { CedarCopilot } from "cedar-os";
-import AppointmentsPage from "./pages/AppointmentsPage";
-import PatientInterface from "./pages/PatientInterface";
 import OnboardingQuestionnaire from "./pages/OnboardingQuestionaire";
-import SignIn from "./pages/SignIn";
-import DoctorDashboard from "./pages/DoctorDashboard";
-import DoctorPatientView from "./pages/DoctorPatientView";
-import OnboardingThankYou from "./pages/OnboardingThankYou";
 
 export default function Home() {
   return (
-    <SignIn />
+    <OnboardingQuestionnaire />
   );
 }
